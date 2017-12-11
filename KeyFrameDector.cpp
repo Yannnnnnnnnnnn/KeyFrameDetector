26ec187323cd16a6e73abb36640ea6becd214fed
Yan <KevinYan37@163.com>
2017-12-11 19:15:43 +0800

    update

--- KeyFrameDector.cpp
+++ KeyFrameDector.cpp
@@ -32,30 +32,25 @@ string int2str(const int int_temp)
     return stream.str();
 }
 
-double calcClarity(const Mat &image)
-{
-    Mat dst;
-    int kernel_size = 3;
-    int scale = 1;
-    int delta = 0;
-    int ddepth = CV_16S;
-    
-    Laplacian(image,dst,ddepth,kernel_size,scale,delta,BORDER_DEFAULT);
-    Scalar mean,stddev;
-    meanStdDev(dst,mean,stddev);
-    return stddev.val[0];
-
-}
-
 int main( int argc, char** argv )
 {
+    if(argc<3)
+    {
+        cerr<<"not enough parameter"<<endl;
+        cerr<<"argv[1] for input video"<<endl;
+        cerr<<"argv[2] for output key frames"<<endl;
+        cerr<<"argv[3] for move pixel"<<endl;
+        cerr<<"argv[4] for find clear images near by"<<endl;
+        return 0;
+    }
+
     VideoCapture cap;
     TermCriteria termcrit(TermCriteria::COUNT|TermCriteria::EPS,20,0.03);
     Size subPixWinSize(10,10), winSize(31,31);
     const int MAX_COUNT = 500;
     
-    std::string video_file_path = "1.mp4";
-    std::string output_file_path = "/home/yan/Desktop/KeyFrameDetector/build/output/";
+    std::string video_file_path = argv[1];
+    std::string output_file_path = argv[2];
 
     cap.open(video_file_path);
     if( !cap.isOpened() )
@@ -64,31 +59,27 @@ int main( int argc, char** argv )
         return 0;
     }
     
-    //int frame_width = cv::GetCaptureProperty(cap,CV_CAP_PROP_FRAME_WIDTH);
-    //int frame_height = cv::GetCaptureProperty(cap,CV_CAP_PROP_FRAME_HEIGHT);
+    int current_frame_index = 0;
+    std::vector<int> key_frame_indexs;
+    std::vector<double> frames_clearity;
 
-    Mat image,current_frame;
-    Mat key_frame;
-    int key_frame_count = 0;
+    Mat key_frame,current_frame;
     Mat current_frame_gray,key_frame_gray;
     vector<Point2f> key_frame_points,current_frame_points;
     vector<Point2f> cornor_points;
 
-    int a= 0;
     while(1)
     {
-        cout<<a++<<endl;
-
         cap >> current_frame;
         if( current_frame.empty() )
             break;
-
-        
-            
+                
         //init
         //look as the first frame as the first key frame
         if(key_frame.empty())
         {
+            key_frame_indexs.push_back(current_frame_index);
+
             current_frame.copyTo(key_frame);
             cvtColor(key_frame, key_frame_gray, COLOR_BGR2GRAY);
 
@@ -144,33 +135,11 @@ int main( int argc, char** argv )
 
             std::cout<<"Moving: "<<mean_all<<"\t"<<mean_filter<<std::endl;
 
-
             //dealing
-            if(mean_filter<50)
-            {
-                /*
-                //compare blurry and select the clear one
-                double key_frame_clarity_index = calcClarity(key_frame_gray);
-                double current_frame_clarity_index = calcClarity(current_frame_gray);
-                cout<<"clarity:"<<key_frame_clarity_index<<"\t"<<current_frame_clarity_index<<endl;
-                if(current_frame_clarity_index>key_frame_clarity_index)
-                {
-                    cv::swap(key_frame, current_frame);
-                    cvtColor(key_frame, key_frame_gray, COLOR_BGR2GRAY);
-    
-                    cornor_points.clear();
-                    goodFeaturesToTrack(key_frame_gray, cornor_points, MAX_COUNT, 0.01, 10);
-                    cornerSubPix(key_frame_gray, cornor_points, subPixWinSize, Size(-1,-1), termcrit);
-    
-                    key_frame_points.clear();
-                    key_frame_points.assign(cornor_points.begin(),cornor_points.end());
-                }
-                */
-            }
-            else
+            if(mean_filter>=50)
             {
                 //save the keyframe and set the current frame as the next key frame
-                string image_name = int2str(key_frame_count++)+".jpg";
+                string image_name = int2str(current_frame_index)+".jpg";
                 cv::imwrite(output_file_path+image_name,key_frame);
 
                 cv::swap(key_frame, current_frame);
@@ -183,8 +152,13 @@ int main( int argc, char** argv )
                 key_frame_points.clear();
                 key_frame_points.assign(cornor_points.begin(),cornor_points.end());
             }
-
         }
+        current_frame_index++;
     }
+
+    cap.release();
+
+
+
     return 0;
 }
